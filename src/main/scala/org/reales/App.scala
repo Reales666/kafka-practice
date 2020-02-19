0cf13fcdfc1ece8a11b92822f87f89b9b5f13077
Alberto Reales <alberto.realesdiaz@thalesgroup.com>
2020-02-19 15:04:26 +0100

    Fixed main function code

--- src/main/scala/org/reales/App.scala
+++ src/main/scala/org/reales/App.scala
@@ -1,20 +1,20 @@
 package org.reales
 
 import org.apache.spark.sql.SparkSession
+import org.reales.consumer.KafkaConsumer
 
-/**
- * Hello world!
- *
- */
 object App
 {
-	def main(strings: Array[String]): Unit =
+	def main(args: Array[String]): Unit =
 	{
 		val spark = SparkSession
 			.builder()
 			.appName("Spark Kafka Application")
+			.config("spark.master", "local")
     		.getOrCreate()
 		
+		val kafkaConsumer = new KafkaConsumer(spark, args(0))
 		
+		kafkaConsumer.subscribeKafkaTopic(args(1), args(2))
 	}
 }
